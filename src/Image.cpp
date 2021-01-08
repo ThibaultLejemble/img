0b178e18de7c7eada973c47a8b547d16fe7ead27
T. Lejemble <thibault.lejemble@irit.fr>
2021-01-08 14:11:45 +0100

    add stb

--- src/Image.cpp
+++ src/Image.cpp
@@ -4,9 +4,9 @@
 
 #define STB_IMAGE_IMPLEMENTATION
 #define STBI_ONLY_PNG
-#include "internal/stb_image.h"
+#include "stb_image.h"
 #define STB_IMAGE_WRITE_IMPLEMENTATION
-#include "internal/stb_image_write.h"
+#include "stb_image_write.h"
 
 namespace pcp {
 
