2c84048977577954b9a0500b01011304080e9634
T. Lejemble <thibault.lejemble@irit.fr>
2021-03-01 21:35:02 +0100

    add example2

---
+++ examples/example2_binary.cpp
@@ -0,0 +1,26 @@
+#include <img/Image.h>
+#include <img/BinaryImage.h>
+
+#include <iostream>
+
+using namespace img;
+
+int main()
+{
+    Image image;
+    bool ok = image.load("example1_fractal.png");
+    if(!ok)
+    {
+        std::cout << "Failed to load image 'example1_fractal.png'" << std::endl;
+        return 1;
+    }
+
+    // RGB to binary
+    BinaryImage binary = image;
+
+    // binary to RGB (black and white)
+    image = binary;
+
+    ok = image.save("example2_binary.png");
+    return !ok;
+}
