bafd4eceac809c5af641b9040923704af68ea387
T. Lejemble <thibault.lejemble@irit.fr>
2021-03-02 02:36:42 +0100

    convert

--- src/img/Operations/Convert.cpp
+++ src/img/Operations/Convert.cpp
@@ -2,6 +2,7 @@
 #include <img/Image/Image.h>
 #include <img/Image/BinaryImage.h>
 #include <img/Image/GrayScaleImage.h>
+#include <img/Image/LabelImage.h>
 
 namespace img {
 
@@ -25,13 +26,25 @@ void convert(const Image& rgb, GrayScaleImage& gray)
     }
 }
 
+void convert(const Image& rgb, LabelImage& labels)
+{
+    labels.resize(rgb.height(), rgb.width());
+
+    for(int k=0; k<rgb.size(); ++k)
+    {
+        labels(k) = (rgb(k).head<3>().sum() / 3 > 0.5);
+    }
+}
+
+// -----------------------------------------------------------------------------
+
 void convert(const BinaryImage& binary, Image& rgb)
 {
     rgb.resize(binary.height(), binary.width());
 
     for(int k=0; k<binary.size(); ++k)
     {
-        rgb(k) = binary(k) ? Image::Color(1,1,1,1) : Image::Color(0,0,0,1);
+        rgb(k) = Image::Color(binary(k),binary(k),binary(k),1);
     }
 }
 
@@ -41,9 +54,21 @@ void convert(const BinaryImage& binary, GrayScaleImage& gray)
 
     for(int k=0; k<binary.size(); ++k)
     {
-        gray(k) = binary(k) ? 1 : 0;
+        gray(k) = binary(k);
     }
 }
+
+void convert(const BinaryImage& binary, LabelImage& labels)
+{
+    labels.resize(binary.height(), binary.width());
+
+    for(int k=0; k<binary.size(); ++k)
+    {
+        labels(k) = binary(k);
+    }
+}
+
+// -----------------------------------------------------------------------------
 
 void convert(const GrayScaleImage& gray, Image& rgb)
 {
@@ -61,7 +86,66 @@ void convert(const GrayScaleImage& gray, BinaryImage& binary)
 
     for(int k=0; k<gray.size(); ++k)
     {
-        binary(k) = gray(k) > 0.5 ? 1 : 0;
+        binary(k) = gray(k) > 0.5;
+    }
+}
+
+void convert(const GrayScaleImage& gray, LabelImage& labels)
+{
+    labels.resize(gray.height(), gray.width());
+
+    for(int k=0; k<gray.size(); ++k)
+    {
+        labels(k) = gray(k) > 0.5;
+    }
+}
+
+// -----------------------------------------------------------------------------
+
+void convert(const LabelImage& labels, Image& rgb)
+{
+    rgb.resize(labels.height(), labels.width());
+
+    int label_min = std::numeric_limits<int>::max();
+    int label_max = std::numeric_limits<int>::min();
+    for(int k=0; k<labels.size(); ++k)
+    {
+        label_min = std::min(label_min, labels(k));
+        label_max = std::max(label_max, labels(k));
+    }
+
+    for(int k=0; k<labels.size(); ++k)
+    {
+        const float t = float(labels(k)-label_min)/(label_max-label_min);
+        rgb(k) = Image::Color(t,t,t,1);
+    }
+}
+
+void convert(const LabelImage& labels, BinaryImage& binary)
+{
+    binary.resize(labels.height(), labels.width());
+
+    for(int k=0; k<labels.size(); ++k)
+    {
+        binary(k) = labels(k) >= 0;
+    }
+}
+
+void convert(const LabelImage& labels, GrayScaleImage& gray)
+{
+    gray.resize(labels.height(), labels.width());
+
+    int label_min = std::numeric_limits<int>::max();
+    int label_max = std::numeric_limits<int>::min();
+    for(int k=0; k<labels.size(); ++k)
+    {
+        label_min = std::min(label_min, labels(k));
+        label_max = std::max(label_max, labels(k));
+    }
+
+    for(int k=0; k<labels.size(); ++k)
+    {
+        gray(k) = float(labels(k)-label_min)/(label_max-label_min);
     }
 }
 
