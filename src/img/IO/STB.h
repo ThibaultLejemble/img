8f342f5f6cc8a36adcfab4e57ccaf42f353520d0
Thibault Lejemble <thibault.le@live.fr>
2021-05-30 18:04:52 +0200

    start stb cleaning

--- src/img/IO/STB.h
+++ src/img/IO/STB.h
@@ -2,6 +2,6 @@
 
 #define STB_IMAGE_IMPLEMENTATION
 #define STBI_ONLY_PNG
-#include <img/IO/stb/stb_image_header_only.h>
+#include <img/IO/stb/stb_image_header_only2.h>
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include <img/IO/stb/stb_image_write_header_only.h>
