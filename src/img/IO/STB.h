cd2a0fc654fa0472c28394337c551f93ba171d2f
Thibault Lejemble <thibault.le@live.fr>
2021-05-31 21:47:20 +0200

    clean

--- src/img/IO/STB.h
+++ src/img/IO/STB.h
@@ -1,7 +1,4 @@
 #pragma once
 
-#define STB_IMAGE_IMPLEMENTATION
-#define STBI_ONLY_PNG
 #include <img/IO/stb/stb_image_header_only2.h>
-#define STB_IMAGE_WRITE_IMPLEMENTATION
 #include <img/IO/stb/stb_image_write_header_only2.h>
