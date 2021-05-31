a67d111948b4765fd3370579dde5552f7c98538b
Thibault Lejemble <thibault.le@live.fr>
2021-05-31 23:54:41 +0200

    update example 1

--- examples/example1_fractal.cpp
+++ examples/example1_fractal.cpp
@@ -1,28 +1,29 @@
-#include <img/Image/Image.h>
-#include <img/IO/Image_save.h>
+#include <img/Image/ImageT.h>
 
 using namespace img;
 
 int main()
 {
-    Image image(610,1080);
+    ImageRGBf image(610,1080);
 
-    const Image::Color black(0,0,0,1);
-    const Image::Color green(0,1,0,1);
-    const Image::Color white(1,1,1,1);
-    int max_iteration = 100;
+    const auto black = ImageRGBf::Color(0,0,0);
+    const auto green = ImageRGBf::Color(0,1,0);
+    const auto white = ImageRGBf::Color(1,1,1);
+
+    constexpr auto max_iteration = 100;
+
     for(int i=0; i<image.height(); ++i)
     {
         for(int j=0; j<image.width(); ++j)
         {
-            const float x0 = float(j)/(image.width()-1)  * 3.5f - 2.5f;
-            const float y0 = float(i)/(image.height()-1) * 2.0f - 1.0f;
-            float x = 0.f;
-            float y = 0.f;
-            int iteration = 0;
+            const auto x0 = float(j)/(image.width()-1)  * 3.5f - 2.5f;
+            const auto y0 = float(i)/(image.height()-1) * 2.0f - 1.0f;
+            auto x = 0.f;
+            auto y = 0.f;
+            auto iteration = 0;
             while(x*x + y*y <= 4 && iteration < max_iteration)
             {
-                const float xtemp = x*x - y*y + x0;
+                const auto xtemp = x*x - y*y + x0;
                 y = 2*x*y + y0;
                 x = xtemp;
                 ++iteration;
@@ -33,7 +34,7 @@ int main()
             }
             else
             {
-                const float t = float(iteration)/max_iteration;
+                const auto t = float(iteration)/max_iteration;
                 image(i,j) = (1.f-t)*black + t*green;
             }
         }
