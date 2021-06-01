8557780e8fcfd026439253a2d8beac2fec671a2d
Thibault Lejemble <thibault.le@live.fr>
2021-06-01 23:25:42 +0200

    fix

--- examples/example0_test.cpp
+++ examples/example0_test.cpp
@@ -25,32 +25,32 @@ int main()
         }
     }
 
-    save("exmaple0_Gi.png",    ImageGi   (image));
-    save("exmaple0_Gf.png",    ImageGf   (image));
-    save("exmaple0_Gd.png",    ImageGd   (image));
-    save("exmaple0_GAi.png",   ImageGAi  (image));
-    save("exmaple0_GAf.png",   ImageGAf  (image));
-    save("exmaple0_GAd.png",   ImageGAd  (image));
-    save("exmaple0_RGBi.png",  ImageRGBi (image));
-    save("exmaple0_RGBf.png",  ImageRGBf (image));
-    save("exmaple0_RGBd.png",  ImageRGBd (image));
-    save("exmaple0_RGBAi.png", ImageRGBAi(image));
-    save("exmaple0_RGBAf.png", ImageRGBAf(image));
-    save("exmaple0_RGBAd.png", ImageRGBAd(image));
+    save("example0_Gi.png",    ImageGi   (image));
+    save("example0_Gf.png",    ImageGf   (image));
+    save("example0_Gd.png",    ImageGd   (image));
+    save("example0_GAi.png",   ImageGAi  (image));
+    save("example0_GAf.png",   ImageGAf  (image));
+    save("example0_GAd.png",   ImageGAd  (image));
+    save("example0_RGBi.png",  ImageRGBi (image));
+    save("example0_RGBf.png",  ImageRGBf (image));
+    save("example0_RGBd.png",  ImageRGBd (image));
+    save("example0_RGBAi.png", ImageRGBAi(image));
+    save("example0_RGBAf.png", ImageRGBAf(image));
+    save("example0_RGBAd.png", ImageRGBAd(image));
 
     {
-        {ImageGi    image2; load("exmaple0_Gi.png",    image2); save("exmaple0_Gi2.png",    image2);}
-        {ImageGf    image2; load("exmaple0_Gf.png",    image2); save("exmaple0_Gf2.png",    image2);}
-        {ImageGd    image2; load("exmaple0_Gd.png",    image2); save("exmaple0_Gd2.png",    image2);}
-        {ImageGAi   image2; load("exmaple0_GAi.png",   image2); save("exmaple0_GAi2.png",   image2);}
-        {ImageGAf   image2; load("exmaple0_GAf.png",   image2); save("exmaple0_GAf2.png",   image2);}
-        {ImageGAd   image2; load("exmaple0_GAd.png",   image2); save("exmaple0_GAd2.png",   image2);}
-        {ImageRGBi  image2; load("exmaple0_RGBi.png",  image2); save("exmaple0_RGBi2.png",  image2);}
-        {ImageRGBf  image2; load("exmaple0_RGBf.png",  image2); save("exmaple0_RGBf2.png",  image2);}
-        {ImageRGBd  image2; load("exmaple0_RGBd.png",  image2); save("exmaple0_RGBd2.png",  image2);}
-        {ImageRGBAi image2; load("exmaple0_RGBAi.png", image2); save("exmaple0_RGBAi2.png", image2);}
-        {ImageRGBAf image2; load("exmaple0_RGBAf.png", image2); save("exmaple0_RGBAf2.png", image2);}
-        {ImageRGBAd image2; load("exmaple0_RGBAd.png", image2); save("exmaple0_RGBAd2.png", image2);}
+        {ImageGi    image2; load("example0_Gi.png",    image2); save("example0_Gi2.png",    image2);}
+        {ImageGf    image2; load("example0_Gf.png",    image2); save("example0_Gf2.png",    image2);}
+        {ImageGd    image2; load("example0_Gd.png",    image2); save("example0_Gd2.png",    image2);}
+        {ImageGAi   image2; load("example0_GAi.png",   image2); save("example0_GAi2.png",   image2);}
+        {ImageGAf   image2; load("example0_GAf.png",   image2); save("example0_GAf2.png",   image2);}
+        {ImageGAd   image2; load("example0_GAd.png",   image2); save("example0_GAd2.png",   image2);}
+        {ImageRGBi  image2; load("example0_RGBi.png",  image2); save("example0_RGBi2.png",  image2);}
+        {ImageRGBf  image2; load("example0_RGBf.png",  image2); save("example0_RGBf2.png",  image2);}
+        {ImageRGBd  image2; load("example0_RGBd.png",  image2); save("example0_RGBd2.png",  image2);}
+        {ImageRGBAi image2; load("example0_RGBAi.png", image2); save("example0_RGBAi2.png", image2);}
+        {ImageRGBAf image2; load("example0_RGBAf.png", image2); save("example0_RGBAf2.png", image2);}
+        {ImageRGBAd image2; load("example0_RGBAd.png", image2); save("example0_RGBAd2.png", image2);}
     }
 
 
